fc3db3333995928d648febe67eaae25260a9408c
Mafifa <120286152+Mafifa@users.noreply.github.com>
2024-03-08 22:07:34 -0400

    Now the hook call a service in another file

--- src/hooks/Hooks.js
+++ src/hooks/Hooks.js
@@ -1,15 +1,15 @@
 import { useEffect, useMemo, useState } from 'react'
+import { taketQuote } from '../services/service'
 import mock from '../hooks/mock.json'
 
-const API_KEY = 'WQuGLKoVcAvjLO4kLvSVpQ==0AmOmP7D25PDkmVm'
 const IMAGE_API_KEY = '7u1BHLA6aYv30TGOfP7zK2pCqtqLaRf0xG3AyhBa1vFhDwvqPgAwhNWj'
-const URL = 'https://api.api-ninjas.com/v1/quotes?category='
 
 export function Quotes () {
   const [quote, setQuote] = useState(mock)
   const [stringQuote, setQuoteString] = useState('')
   const [stringAuthor, setAuthor] = useState('')
   const [topic, setTopic] = useState('inspirational')
+  const [load, setLoad] = useState(false)
 
   // TO GET RANDOM TOPICS
   const TOPICS = useMemo(() => {
@@ -31,18 +31,10 @@ export function Quotes () {
     setTopic(newTopic)
   }, [TOPICS])
 
-  const getQuote = () => {
-    fetch(URL + topic, {
-      method: 'GET',
-      headers: { 'X-Api-Key': API_KEY }
-    })
-      .then(response => response.json())
-      .then(json => {
-        setQuote(json)
-        console.log(json)
-      })
-      .catch(error => console.error('Error fetching quotes:', error))
-    console.log('Quote Called')
+  const getQuote = async () => {
+    const newQuote = await taketQuote({ topic })
+    console.log(newQuote)
+    setQuote(newQuote)
   }
 
   return { stringAuthor, stringQuote, getQuote }
