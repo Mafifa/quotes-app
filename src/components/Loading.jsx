ed87f2ee355e2be96e2117138b9bf44567641c71
Mafifa <120286152+Mafifa@users.noreply.github.com>
2024-03-08 22:27:23 -0400

    Add the Loading component

---
+++ src/components/Loading.jsx
@@ -0,0 +1,10 @@
+import '../assets/styles/Loading.css'
+
+export function Loading () {
+  return (
+    <div className='container size-16'>
+      <div className='half' />
+      <div className='half' />
+    </div>
+  )
+}
